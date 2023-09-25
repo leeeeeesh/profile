23b0a6bab23ef249c484128c6120f50bec444544
이상현 <sh941217@naver.com>
2023-09-26 02:14:32 +0900

    test0926

--- src/components/mobileabout/MobileAbout.tsx
+++ src/components/mobileabout/MobileAbout.tsx
@@ -9,10 +9,11 @@ export default function MobileAbout() {
         <h2 className='title_name'>About</h2>
         <div id={styles.about_img}></div>
         <div id={styles.about_text}>
-          <p>TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT TEXT</p>
-          <p className={styles.about_contact}><img src="/images/about_address.png" alt="주소이미지" />서울시 00구 00동</p>
-          <p className={styles.about_contact}><img src="/images/about_tell.png" alt="전화이미지" />010 - 1234 - 5678</p>
-          <p className={styles.about_contact}><img src="/images/about_mail.png" alt="메일이미지" />abcd1234@gmail.com</p>
+          <p>제가 생각했던 것을 직접 화면에 구현하는 것이 재미있습니다.
+            <br/>저의 이름처럼 새로운 것을 두려워하지 않고 항상 빛날 수 있는 프론트엔드 개발자가 되겠습니다.</p>
+          <p className={styles.about_contact}><img src="/images/about_address.png" alt="주소이미지" />서울시 노원구 공릉동</p>
+          <p className={styles.about_contact}><img src="/images/about_tell.png" alt="전화이미지" />010-4024-2694</p>
+          <p className={styles.about_contact}><img src="/images/about_mail.png" alt="메일이미지" />sh941217@gmail.com</p>
         </div>
 
         
--- src/page/MobileHeader.tsx
+++ src/page/MobileHeader.tsx
@@ -5,7 +5,8 @@ export default function MobileHeader() {
   return (
     <div id={styles.header_wrap}>
       <header>
-        <div id={styles.test_box}></div>
+        {/* <div id={styles.test_box}></div> */}
+        <h1 id={styles.logo}>Logo</h1>
       </header>
     </div>
   )
